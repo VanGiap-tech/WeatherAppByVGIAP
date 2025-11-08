cfdb4eb967c65e2d61bbf0f802f0dbaa0fe067fd
VanGiap <hoanggiap2k4@gmail.com>
2025-11-08 21:39:44 +0700

    Thêm các icon logo va reponsive cho dien thoai

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Weather from './components/Weather'
-import logo from './assets/boy.png'
+import logo from './assets/Creative.jpg'
 
 const App = () => {
   return (
     <div className='app'>
       
       <Weather />
-      <h2>Weather By Hoang Giap <span><img src={logo}alt="" /></span> </h2>
-      
+      <div className='intro-app'>
+<h2>Weather By Hoang Giap  </h2>
+      <span><img src={logo}alt="" /></span>
+      </div>
     </div>
    
   )
